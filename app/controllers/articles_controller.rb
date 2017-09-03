2040da8f94043dc9ae1f5cfcc7d63c74ac2a7c23
Stefan Jenkins <stefan.jenkins@gmail.com>
2017-09-03 07:25:01 -0400

    Finished Comments
    Finished comments, managed to get github to work.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    #@comment = Comment.new
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+=begin one way of doing articles
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    redirect_to article_path(@article)
+
+=end
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' has been created!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    flash.notice = "Article '#{@article.title}' has been removed!"
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+
+end
